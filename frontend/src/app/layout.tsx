d6bb6d6159ece9175900d22d8d4d452ab5fd995a
asifrahaman13 <asifrahaman162@gmail.com>
2024-07-02 16:37:03 +0530

    ⚒️ fix: fix docker builds

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import ApolloClientProvider from './apolloProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <ApolloClientProvider>{children}</ApolloClientProvider>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }

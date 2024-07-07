9de25c4ba55fffd83da8269dc89a1723f24f247d
asifrahaman13 <asifrahaman162@gmail.com>
2024-07-07 21:24:57 +0530

    🐍 wip: add sqlite support from frontend

---
+++ frontend/src/constants/static/sqlite/sqliteTexts.ts
@@ -0,0 +1,8 @@
+const sqliteTexts = [
+  'Can you tell me the total number of users in the users table.',
+  'I want bar graph on the users table to display the marks of the users. The x axis should be the address Group it by the address.',
+  'I want line graph of the price of all the products in the sales table. y axis should contain the qrantity, x axis the sales data, labels will the the product names. Fetch all the entries',
+  'I want bar graph on the users table to display the marks of the users. The x axis should be the name of the user y axis the marks',
+];
+
+export { sqliteTexts };

28e98a7fd571c85847cd4c9c86d2c92a772a9a94
asifrahaman13 <asifrahaman162@gmail.com>
2024-07-22 00:39:20 +0530

    🎉 wip: altered few texts

--- frontend/src/constants/static/staticTexts/staticTexts.ts
+++ frontend/src/constants/static/staticTexts/staticTexts.ts
@@ -1,8 +1,8 @@
 const conversationStaticTexts = [
-  'Can you tell me the total number of users in the users table.',
-  'I want bar graph on the users table to display the marks of the users. The x axis should be the address Group it by the address.',
-  'I want line graph of the price of all the products in the sales table. y axis should contain the qrantity, x axis the sales data, labels will the the product names. Fetch all the entries',
-  'I want bar graph on the users table to display the marks of the users. The x axis should be the name of the user y axis the marks',
+  'Run manual query from sidebar to ensure the result is correct',
+  'Its recommened to train the system whenever you  get accurate resuls',
+  'Address clearly. The more details you provide, the better the results',
+  'Try to give as much details as possible',
 ];
 
 const team = [

411765b85ada90a3c426e6dd8894e83a89191604
Asif Rahaman <asifrahaman162@gmail.com>
2025-03-09 13:26:00 +0530

    wip: all graphs of same length

--- frontend/src/app/dashboard/saved-graphs/page.tsx
+++ frontend/src/app/dashboard/saved-graphs/page.tsx
@@ -6,12 +6,11 @@ import React, { useEffect } from 'react';
 const Page = () => {
   const [presignedUrls, setPresignedUrls] = React.useState([]);
   useEffect(() => {
-    console.log('Saved graphs page');
-
     async function fetchAllPresignedUrls() {
       const token = localStorage.getItem('accessToken') || '';
       const response = await file_interface.presignedUrls(token);
       if (response?.code === 200) {
+        console.log(response.data.url)
         setPresignedUrls(response.data.url);
       }
     }
@@ -23,10 +22,10 @@ const Page = () => {
       <div className="flex flex-col">
         <div className="text-xl font-medium px-4">My saved graphs</div>
         <div className="flex flex-col h-full overflow-y-scroll no-scrollbar py-12">
-          <div className="flex flex-wrap px-4">
+          <div className="flex flex-wrap px-4 gap-4">
             {presignedUrls.map((item, index) => (
-              <div key={index} className="p-2">
-                <img src={item} alt="" />
+              <div key={index} className="p-2 ">
+                <img src={item} alt="" className='w-64 h-64'/>
               </div>
             ))}
           </div>
